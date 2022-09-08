0ff36055c069b0cceb60414c58e8bceb23055620
sammimac95 <112788421+sammimac95@users.noreply.github.com>
2022-09-08 08:59:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,55 +5,40 @@ let hotButton = document.getElementById("hot-button");
 let coldButton = document.getElementById("cold-button");
 let evaluate = document.getElementById("evaluate-answer");
 let displayTempNum = document.getElementById('temp-number');
+let numCorrect = 0;
+let numIncorrect = 0;
+let scorePercentage = 0;
+let totalAnswered = 0;
+let numCorrectDisplay = document.getElementById('num-correct');
+let numIncorrectDisplay = document.getElementById('num-incorrect');
+let totalAnsweredDisplay = document.getElementById('num-answered');
+let scorePercentageDisplay = document.getElementById('score-percentage');
 
 
-tempNumber.value = Math.floor(Math.random() * 100);
-tempDisplay.innerText = "Tempature is: "
-displayTempNum.innerText = tempNumber.value + String.fromCharCode(176) + "F"
-
+newNumber();
 
 
 resetButton.addEventListener("click", function () {
-    tempNumber.value = Math.floor(Math.random() * 100);
-    displayTempNum.innerText = tempNumber.value + String.fromCharCode(176) + "F"
-    tempDisplay.innerText = "Tempature is: "
+    newNumber();
     evaluate.innerText = ""
     document.getElementById("evaluate-answer").style.padding = '0px';
 })
 
 hotButton.addEventListener("click", function () {
     if ((tempNumber.value) >= 60) {
-        evaluate.innerText = "Correct!"
-        document.getElementById("evaluate-answer").style.backgroundColor = "green";
-        document.getElementById("evaluate-answer").style.padding = '30px';
-        playCorrect();
-
+        correct();
     }
-    if ((tempNumber.value) < 60) {
-        evaluate.innerText = "Incorrect!"
-        document.getElementById("evaluate-answer").style.backgroundColor = "red";
-        document.getElementById("evaluate-answer").style.padding = '30px';
-        playIncorrect();
-
+    else {
+        incorrect();
     }
 })
 
 coldButton.addEventListener("click", function () {
     if ((tempNumber.value) < 60) {
-        evaluate.innerText = "Correct!"
-        document.getElementById("evaluate-answer").style.backgroundColor = "green";
-        document.getElementById("evaluate-answer").style.padding = '30px';
-        playCorrect();
-
-
+        correct()
     }
-    if ((tempNumber.value) >= 60) {
-        evaluate.innerText = "Incorrect!"
-        document.getElementById("evaluate-answer").style.backgroundColor = "red";
-        document.getElementById("evaluate-answer").style.padding = '30px';
-        playIncorrect();
-
-
+    else {
+        incorrect();
     }
 })
 
@@ -66,4 +51,43 @@ function playCorrect() {
 function playIncorrect() {
     var audio = document.getElementById("incorrect-sound");
     audio.play();
+}
+
+
+function correct() {
+    evaluate.innerText = "Correct!"
+    document.getElementById("evaluate-answer").style.backgroundColor = "green";
+    document.getElementById("evaluate-answer").style.padding = '30px';
+    numCorrect += 1
+    totalAnswered += 1
+    numCorrectDisplay.innerText = numCorrect;
+    totalAnsweredDisplay.innerText = totalAnswered
+    calculateScorePercentage();
+    playCorrect();
+    newNumber();
+}
+
+function incorrect() {
+    evaluate.innerText = "Incorrect!"
+    document.getElementById("evaluate-answer").style.backgroundColor = "red";
+    document.getElementById("evaluate-answer").style.padding = '30px';
+    numIncorrect += 1
+    totalAnswered += 1
+    numIncorrectDisplay.innerText = numIncorrect;
+    totalAnsweredDisplay.innerText = totalAnswered
+    calculateScorePercentage();
+    playIncorrect();
+    newNumber();
+}
+
+function newNumber() {
+    tempNumber.value = Math.floor(Math.random() * 100);
+    tempDisplay.innerText = "Tempature is: "
+    displayTempNum.innerText = tempNumber.value + String.fromCharCode(176) + "F"
+}
+
+function calculateScorePercentage() {
+    let percentage = Math.round((numCorrect / totalAnswered) * 100);
+    // Math.round(percentage, 2
+    scorePercentageDisplay.innerHTML = percentage + "%";
 }
